6cd5c4cfc50e637763dd6c52bb93c3e38e353ca1
Beyzabay <beyza.bay@icloud.com>
2024-05-10 16:31:02 +0300

    component belonging to the products was created and the products were listed on the home page

--- client/src/Components/Products.jsx
+++ client/src/Components/Products.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from "react";
 
 function Products() {
   return (
-    <div>Products deneme</div>
-  )
+    <div className="container">
+      <div className="flex gap-7 flex-wrap justify-between">
+        <div class="max-w-24 bg-white border border-gray-200 rounded-lg shadow">
+          <a href="/">
+            <img
+              class="rounded-t-lg"
+              src="https://cdn.yemek.com/mncrop/940/625/uploads/2018/02/arnavut-cilegi.jpg"
+              alt=""
+            />
+          </a>
+          <div class="px-3">
+            <a href="/">
+              <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900">
+                Çilek
+              </h5>
+            </a>
+            <p class="mb-3 font-normal dark:text-gray-400">24₺</p>
+          </div>
+        </div>
+        <div class="max-w-24 bg-white border border-gray-200 rounded-lg shadow">
+          <a href="/">
+            <img
+              class="rounded-t-lg"
+              src="https://cdn.yemek.com/mncrop/940/625/uploads/2018/02/arnavut-cilegi.jpg"
+              alt=""
+            />
+          </a>
+          <div class="px-3">
+            <a href="/">
+              <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900">
+                Çilek
+              </h5>
+            </a>
+            <p class="mb-3 font-normal dark:text-gray-400">24₺</p>
+          </div>
+        </div>
+        <div class="max-w-24 bg-white border border-gray-200 rounded-lg shadow">
+          <a href="/">
+            <img
+              class="rounded-t-lg"
+              src="https://cdn.yemek.com/mncrop/940/625/uploads/2018/02/arnavut-cilegi.jpg"
+              alt=""
+            />
+          </a>
+          <div class="px-3">
+            <a href="/">
+              <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900">
+                Çilek
+              </h5>
+            </a>
+            <p class="mb-3 font-normal dark:text-gray-400">24₺</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Products
+export default Products;
